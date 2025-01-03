215013c29fb7c0fa68a30dcf51dbf8a1fa1e6b5f
soyung sim <simsy1024@gmail.com>
2025-01-03 13:03:14 +0900

    TodoList

--- public/index.js
+++ public/index.js
@@ -1,6 +1,10 @@
 /* 1. https://jsonplaceholder.typicode.com/todos 로부터 데이터를 불러와서 추가해주는 함수 getTodos() 선언 */
 // getTodos()는 추후에 HTML DOM 내용이 완전히 로드되었을 때 실행되어야 합니다.
-async function getTodos() {}
+async function getTodos() {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then((response) => response.json())
+  .then((posts) => console.log(posts))
+}
 
 /* 
   2. 새로운 입력창의 Todo를 Todo 목록에 추가하고, 입력창을 초기화합니다.
@@ -8,11 +12,41 @@ async function getTodos() {}
   - 작성 버튼 클릭 시 addTodo() 함수가 실행됩니다.
   - 입력 창에서 Enter 키 입력시에도 addTodo() 함수가 실행됩니다.
 */
-function addTodo() {}
+function addTodo() {
+  const newLi = document.createElement("li");
+  const newBtn = document.createElement("button");
+  const deleteBtn  = document.createElement("button")
+  const newSpan = document.createElement("span");
+  const todoInput = document.querySelector('#todoInput');
+
+  newLi.appendChild(newBtn);
+  newLi.appendChild(deleteBtn)
+  newLi.appendChild(newSpan);
+  console.log(newLi);
+
+  const todoList = document.querySelector("#todoList");
+
+  newSpan.textContent = todoInput.value;
+
+  todoList.appendChild(newLi);
+  console.log(todoList);
+
+  todoInput.value = "";
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+
+}
 
 /*  3. x 버튼을 클릭하면 클릭한 버튼을 갖는 Todo 항목이 삭제됩니다. */
 // 삭제 함수의 이름 및 모양 변경 가능
-function deleteTodo(item) {}
+function deleteTodo(item) {
+  
+  deleteBtn.addEventListener("click", () => {
+    newLi.classList[item].toggle("complete");
+  });
+}
 
 /* 
  4. Todo 목록 불러오기,  
